09789f1834f46936b1f2a1403e98bc8345ec4e83
LucasMejer <lucas.mm@live.dk>
2025-04-10 12:49:06 +0200

    Tilføjet Lucas Sektion I CSS og JavaScript

--- js/header.js
+++ js/header.js
@@ -1,8 +1,7 @@
+//Lucas Kode Starter Her
 /* Header Menu Kode */
 let menuVariable = document.getElementById("menuUL");
 let logoFade = document.getElementById("logoFade");
-
-
 
 const menuItems = [
     {
@@ -30,10 +29,4 @@ const menuItems = [
 for(let i = 0; i < menuItems.length; i++){
     menuVariable.innerHTML += "<l>" + "<a href=" + menuItems[i].link + ">" + menuItems[i].displayName + "</a>" + "</l>";
 }
-
-setTimeout(codingCourse, 3000);
-
-function codingCourse() {
-    logoFade.classList.toggle("fade");
-}
-
+//Lucas Kode Slutter Her
